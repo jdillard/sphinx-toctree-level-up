38da8e4b9e1e9fa4f5c37d47edca9aa71dc6b7f6
Jared Dillard <jared.dillard@gmail.com>
2026-08-26 11:31:47 -0700

    prepare for v0.1.1 release

--- changelog.md
+++ changelog.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## [Unreleased]
+## 0.1.1 - 2026-08-26
 
 - Add CI coverage for Python 3.10–3.13 and Sphinx 7–9.
 - Add Regression tests for the singlehtml, Texinfo, and man builders.
--- sphinx_toctree_level_up/__init__.py
+++ sphinx_toctree_level_up/__init__.py
@@ -29,7 +29,7 @@ if TYPE_CHECKING:
     from sphinx.environment import BuildEnvironment
     from sphinx.util.typing import ExtensionMetadata, OptionSpec
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __all__ = ['TocLevelUp', 'TocTree', 'setup']
 
 EXTENSION_NAME = 'sphinx_toctree_level_up'
